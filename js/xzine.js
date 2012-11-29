9535c8079407418a971ad399d791e1006142c79e
Delwin Campbell <d@delw.in>
2012-11-28 19:36:40 -0600

    beginnings of the grid, and some kitten silliness

--- js/xzine.js
+++ js/xzine.js
@@ -1,5 +1,24 @@
+function randomInArray(array) {
+	return array[Math.floor(Math.random() * array.length)];
+}
+
+(function( $ ){
+	$.fn.randomize = function() {
+
+		// select a font
+		var font = randomInArray(['Croissant One', 'Julius Sans One', 'Sacramento', 'Oldenburg']);
+		var bg = randomInArray(['#b6cb15', '#c5b9c5', '#bd757f'])
+
+		this.css('background-color', bg);
+		this.find('.headline').css('font-family', font);
+	};
+})( jQuery );
+
 $(document).ready(function() {
+	$('p.background').height($(window).height());
 	$('.site-header').hover(function() {
 		$('nav').toggleClass('open');
 	});
+
+	$('article.text').each(function() { $(this).randomize() });
 });
